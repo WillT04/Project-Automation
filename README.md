50af7e407404aa734902aee02fe23c574905d6b7
William <williamtalwar@gmail.com>
2020-05-01 11:34:04 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Setup
+* Fill in "create_repo.py" with your authentication data and path to your projects folder
+* Add the "create.ps1" script to your powershell profile (run "code $profile" to access powershell profile)
+
+
+# Usage
+1. Run "create" from powershell
+2. Enter the name of the project
+3. Enter the tags, seperated by a comma
+4. Enter if you want the folder to be tracked on git (Y/N)
+5. Enter the IDEs that you want to open with the project
+
+
+# IDE Options
+* IDEA = Intellij IDEA
+* PyCharm = PyCharm
+* Code = VS Code
+
+
+# Tags
+## How to tag
+1. Put the "Tag_Folder.bat" file in D://Program Files (x86)/Tag Folder
+2. Run the "Tag Folder Registry.reg" to add the ability to tag folders to the right click context menu
+
+## Info
+* If you want to tag folders, you must go into the folder and then right click empty space
+* To group folders by tag you need to first add the tag column and then select it in the "Group by" setings (under "View")
+* If you place the "Tag_Folder.bat" file somewhere other than where I said above you will need to change the "Tag Folder Registry.reg" file to fit
+
+# Project Structure
+```
+Name of Project Folder/  (Defined in "create_repo.py")
+├── Tracked
+│   ├── Projects
+│   ├── That
+│   ├── Are
+│   ├── Tracked
+│   ├── On
+│   ├── Bitbucket
+└── Untracked
+    ├── Projects
+    ├── That
+    ├── Are
+    ├── Local
+```
---
+++ create.ps1
@@ -0,0 +1,96 @@
+# Make sure to read README to understand how to use thi script
+# To test comment out the function creation code
+
+function create {
+	# Variables to change
+	$username = "username"
+	$password = "password"
+	$projectpath = "Path/To/Projects"
+	# End of variables to change
+
+	
+
+	# Makes credentials for authentication
+	$credentials = "${username}:$password"
+	$encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials))
+
+	# Asks the users for the variables
+	[String] $name = Read-Host "Name of project"
+	[String] $tags = Read-Host "Tags"
+	[String] $tracked = Read-Host "Tracked (Y/N)"
+	[string] $ide = Read-Host "IDE"
+	Write-Host ""
+
+	# Goes to the Projects folder
+	Set-Location $projectpath 
+
+	# If you want the project to be tracked on bitbucket
+	if ($tracked.ToUpper().Equals("Y")) {
+		Set-Location Tracked
+
+		mkdir $name | Out-Null
+		Set-Location $name
+
+		# Initiates git
+		git init | Out-Null
+
+		# Gets the repo slug (lower case and no spaces)
+		$slugs = $name.ToLower() -split " "
+		$slug = $slugs -join "-"
+
+		# JSON Body of request
+		$body = @"
+		{
+			"scm": "git",
+			"project": {
+				"key": "CD"
+			},
+			"is_private": true,
+			"name": "$name",
+			"fork_policy": "no_public_forks"
+		}
+"@
+
+		# Send the POST request to the Bitbucket api
+		Invoke-WebRequest -UseBasicParsing "https://api.bitbucket.org/2.0/repositories/$username/$slug" -ContentType "application/json" -Method POST -Headers @{ Authorization = "Basic $encodedCredentials" } -Body $body | Out-Null
+
+		# Adds the remote
+		git remote add origin git@bitbucket.org:$username/$slug.git
+
+		# Adds the basic repo files (and ignores desktop.ini)
+		New-Item -path "README.md" -ItemType File | Out-Null
+		Set-Content ".gitignore" -Value "desktop.ini" 
+
+		# Adds all files to bitbucket and sets the origin master as the defualt branch
+		git add .
+		git commit -m "Initial Commit" | Out-Null
+		git push -u origin master 2>&1 | Out-Null
+
+	} else {
+		Set-Location Untracked		
+
+		mkdir $name | Out-Null
+		Set-Location $name 
+	}
+
+	# Sets up the tags so it groups correctly in the file explorer
+	[String[]] $newtags = $tags -split ","
+	[String] $realtags = $newtags -join ";"
+
+	# Adds the tag
+	Set-Content "desktop.ini" -Value "[{F29F85E0-4FF9-1068-AB91-08002B27B3D9}]
+Prop5=31,$realtags"
+
+	attrib +h +s desktop.ini
+
+	# Opens any IDEs if requested
+	if ($ide.ToUpper().Contains("IDEA")) {
+		idea .
+	} 
+	if ($ide.ToUpper().Contains("PYCHARM")) {
+		pycharm .
+	}
+	if ($ide.ToUpper().Contains("CODE")) {
+		code . 
+	}
+}
