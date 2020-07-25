c4308ebd09047e1b5e314645f214b24da0b6b400
William <williamtalwar@gmail.com>
2020-07-25 17:52:12 +0100

    Changed git provider to github and added extra features to when you make a remote repo

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Info
+This project allows for quick setup of new projects. It creates the project folder and tags it. It also allows for automatically adding the project to github, and adding the required files for git (README.md and .gitignore). It also allows you to add github repos to a personal or company account.
+
 # Setup
 * Fill in "create.ps1" with your authentication data and path to your projects folder
 * Add the "create.ps1" script to your PowerShell profile (run "code $profile" to access PowerShell profile)
@@ -8,10 +11,11 @@
 2. Enter the name of the project
 3. Enter the tags, seperated by a comma
 4. Enter if you want the folder to be tracked on git (Y/N)
+4.1. If the project is tracked, you will need to enter some extra details
 
 
 # Tags
-### How to tag
+### How to tag manually
 1. Put the "Tag_Folder.bat" file in D://Program Files (x86)/Tag Folder
 2. Run the "Tag Folder Registry.reg" to add the ability to tag folders to the right click context menu
 
@@ -19,6 +23,7 @@
 * If you want to tag folders, you must go into the folder and then right click empty space
 * To group folders by tag you need to first add the tag column and then select it in the "Group by" setings (under "View")
 * If you place the "Tag_Folder.bat" file somewhere other than where I said above you will need to change the "Tag Folder Registry.reg" file to fit
+* Tag feature developed by Pinjoy
 
 
 # Project Structure
@@ -30,7 +35,7 @@ Name of Project Folder/  (Defined in "create_repo.py")
 │   ├── Are
 │   ├── Tracked
 │   ├── On
-│   └── Bitbucket
+│   └── Github
 └── Untracked
     ├── Projects
     ├── That
--- create.ps1
+++ create.ps1
@@ -1,29 +1,49 @@
 # Make sure to read README to understand how to use this script
 
-function New-Project {
-	# Variables to change
-	$username = "username"
-	$password = "password"
-	$projectpath = "Path/To/Projects"
+# Variables to change
+
+	[String] $projectsPath = "Path/To/Projects"
+	[String] $username = "username"
+	[String] $accessToken = "token"
+	[String] $companyName = "name"
+
 	# End of variables to change
 
-	
-
-	# Makes credentials for authentication
-	$credentials = "${username}:$password"
-	$encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials))
+function New-Project {
+	$base64Token = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $accessToken)))
 
 	# Asks the users for the variables
 	[String] $name = Read-Host "Name of project"
 	[String] $tags = Read-Host "Tags"
 	[String] $tracked = Read-Host "Tracked (Y/N)"
-	Write-Host ""
+	
 
 	# Goes to the Projects folder
-	Set-Location $projectpath 
+	Set-Location $projectsPath 
 
-	# If you want the project to be tracked on bitbucket
+	# If you want the project to be tracked on github
 	if ($tracked.ToUpper().Equals("Y")) {
+		[String] $org = Read-Host "Personal or Company (P/C)"
+		[String] $description = Read-Host "Description (Leave empty for default)"
+		[String] $private = Read-Host "Private (Y/N)"
+		Write-Host ""
+
+		if ($org.ToUpper().Equals("C")) {
+			$org = $companyName
+		} else {
+			$org = $username
+		}
+
+		if ($description.Length -lt 1) {
+			$description = "A new repo"
+		}
+
+		if ($private.ToUpper().Equals("N")) {
+			$private = "false"
+		} else {
+			$private = "true"
+		}
+
 		Set-Location Tracked
 
 		mkdir $name | Out-Null
@@ -32,40 +52,44 @@ function New-Project {
 		# Initiates git
 		git init | Out-Null
 
-		# Gets the repo slug (lower case and no spaces)
-		$slugs = $name.ToLower() -split " "
+		# Gets the repo slug (no spaces)
+		$slugs = $name -split " "
 		$slug = $slugs -join "-"
 
-		# JSON Body of request
-		$body = @"
-		{
-			"scm": "git",
-			"project": {
-				"key": "CD"
-			},
-			"is_private": true,
-			"name": "$name",
-			"fork_policy": "no_public_forks"
+		# Make the repo
+		$headers = @{
+			"Authorization" = "Basic $base64Token"
 		}
-"@
 
-		# Send the POST request to the Bitbucket api
-		Invoke-WebRequest -UseBasicParsing "https://api.bitbucket.org/2.0/repositories/$username/$slug" -ContentType "application/json" -Method POST -Headers @{ Authorization = "Basic $encodedCredentials" } -Body $body | Out-Null
+		$body = @{
+			name = $slug;
+			description = $description;
+			org = $org;
+			private = $private
+		} | ConvertTo-Json
 
+		if ($org.Equals($companyName)) {
+			Invoke-RestMethod -Uri https://api.github.com/orgs/$org/repos -Headers $headers -Body $body -Method Post | Out-Null
+		} else {
+			Invoke-RestMethod -Uri https://api.github.com/user/repos -Headers $headers -Body $body -Method Post | Out-Null
+		}
+		
+		
 		# Adds the remote
-		git remote add origin git@bitbucket.org:$username/$slug.git
+		git remote add origin git@github.com:$org/$slug.git
 
 		# Adds the basic repo files (and ignores desktop.ini)
 		New-Item -path "README.md" -ItemType File | Out-Null
 		Set-Content ".gitignore" -Value "desktop.ini" 
 
-		# Adds all files to bitbucket and sets the origin master as the defualt branch
+		# Adds all files to github and sets the origin master as the defualt branch
 		git add .
 		git commit -m "Initial Commit" | Out-Null
 		git push -u origin master 2>&1 | Out-Null
 
 	} else {
-		Set-Location Untracked		
+		Write-Host ""
+		Set-Location Untracked
 
 		mkdir $name | Out-Null
 		Set-Location $name 
